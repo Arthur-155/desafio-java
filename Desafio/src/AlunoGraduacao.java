bc982f3d042c8546cb13512dee7779663ee6ecd9
Arthur <arthur_bergamo@hotmail.com>
2025-03-30 22:58:17 -0300

    Delete Desafio directory

--- Desafio/src/AlunoGraduacao.java
+++
@@ -1,9 +0,0 @@
-public class AlunoGraduacao extends Aluno {
-    String trabalhoConclusao;
-
-    @Override
-    public void exibirDetalhes() {
-        super.exibirDetalhes();
-        System.out.println(trabalhoConclusao);
-    }
-}
--- Desafio/src/AlunoPosGraduado.java
+++
@@ -1,9 +0,0 @@
-public class AlunoPosGraduado extends Aluno {
-    String orientador;
-
-    @Override
-    public void exibirDetalhes() {
-        super.exibirDetalhes();
-        System.out.println(orientador);
-    }
-}
